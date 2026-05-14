b19989a2a0230191325b45cd23bf883c39e52d95
Arian Naeimi <naeimiarian82@gmail.com>
2026-05-14 15:08:22 +0330

    Delete models directory

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .bs_queue_workers import start_basalam_workers, process_basalam_queue
-from .bs_routine_tasks import prepare_and_queue_basalam_products
-
-__all__ = [
-    'start_basalam_workers',
-    'process_basalam_queue',
-    'prepare_and_queue_basalam_products',
-]
